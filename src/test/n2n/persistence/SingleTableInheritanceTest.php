4ce4ca31d78603a0b49a06b119a3121d6bf212f9
Andreas von Burg <a@von-burg.net>
2023-01-13 15:16:40 +0100

    n2n persistence

---
+++ src/test/n2n/persistence/SingleTableInheritanceTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace n2n\persistence;
+
+use PHPUnit\Framework\TestCase;
+use n2n\persistence\orm\LazyEntityManager;
+
+class SingleTableInheritanceTest extends TestCase {
+
+	function setUp(): void {
+
+	}
+}
