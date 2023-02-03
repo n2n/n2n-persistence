86c2149a247a93a143ee998cf85efac8267025ea
andreasvonburg <a@von-burg.net>
2023-02-03 12:42:44 +0100

    rocket 4 compatibility

--- src/app/n2n/persistence/ext/PersistenceAddOnContext.php
+++ src/app/n2n/persistence/ext/PersistenceAddOnContext.php
@@ -7,14 +7,24 @@ use n2n\persistence\orm\model\EntityModelFactory;
 use n2n\util\magic\impl\SimpleMagicContext;
 use n2n\persistence\orm\EntityManagerFactory;
 
-class PersistenceAddOnContext extends SimpleMagicContext implements AddOnContext {
+class PersistenceAddOnContext implements AddOnContext {
+
+	private SimpleMagicContext $simpleMagicContext;
 
 	function __construct(private PdoPool $pdoPool) {
-		parent::__construct([
+		$this->simpleMagicContext = new SimpleMagicContext([
 			PdoPool::class => $pdoPool,
 			EntityManager::class => fn () => $pdoPool->getEntityManagerFactory()->getExtended(),
 			EntityManagerFactory::class => fn () => $pdoPool->getEntityManagerFactory()
 		]);
+	}
+
+	function hasMagicObject(string $id): bool {
+		return $this->simpleMagicContext->has($id);
+	}
+
+	function lookupMagicObject(string $id, bool $required = true, string $contextNamespace = null): mixed {
+		return $this->lookupMagicObject($id, false, $contextNamespace);
 	}
 
 	function copyTo(AppN2nContext $appN2NContext): void {
