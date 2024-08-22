1fe7879365e9555a385b26bf1342843649fd30ea
andreas <a@von-burg.net>
2024-08-22 14:46:35 +0200

    LifecycleEvent change detection

--- src/app/n2n/persistence/orm/store/action/meta/ActionMetaUtil.php
+++ src/app/n2n/persistence/orm/store/action/meta/ActionMetaUtil.php
@@ -21,7 +21,7 @@ class ActionMetaUtil {
 		}
 
 		foreach ($entityProperty->getEmbeddedEntityPropertyCollection()->getEntityProperties()
-				as $entityProperty) {
+				 as $entityProperty) {
 			array_push($entityProperties, ...$this->flattenEntityProperties($entityProperty));
 		}
 
@@ -48,19 +48,27 @@ class ActionMetaUtil {
 		return false;
 	}
 
-	function containsChangesForAnyBut(EntityProperty $entityProperty): bool {
-		$entityProperties = $this->flattenEntityProperties($entityProperty);
-
-		$markedEntityProperties = $this->actionMeta->getMarkedEntityProperties();
-		$items = $this->actionMeta->getItems();
-		foreach ($entityProperties as $entityProperty) {
-			if (!in_array($entityProperty, $markedEntityProperties)) {
+	private function isEntityPropertyPartOf(EntityProperty $entityProperty, EntityProperty $partOfEntityProperty): bool {
+		do {
+			if ($entityProperty === $partOfEntityProperty) {
 				return true;
 			}
+		} while (null !== $partOfEntityProperty = $partOfEntityProperty->getParent());
 
-			foreach ($items as $item) {
-				if (!in_array($entityProperty, $item->getEntityProperties())) {
-					return true;
+		return false;
+	}
+
+	function containsChangesForAnyBut(EntityProperty $entityProperty): bool {
+		foreach ($this->actionMeta->getMarkedEntityProperties() as $markedEntityProperty) {
+			if (!$this->isEntityPropertyPartOf($entityProperty, $markedEntityProperty)) {
+				return false;
+			}
+		}
+
+		foreach ($this->actionMeta->getItems() as $item) {
+			foreach ($item->getEntityProperties() as $changedEntityProperty) {
+				if (!$this->isEntityPropertyPartOf($entityProperty, $changedEntityProperty)) {
+					return false;
 				}
 			}
 		}
