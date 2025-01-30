19e02730cce3e6da0cdb388c162375b11dccecc6
andreas <a@von-burg.net>
2025-01-30 20:01:11 +0100

    prePersist later

--- src/app/n2n/persistence/orm/store/action/PersistActionPool.php
+++ src/app/n2n/persistence/orm/store/action/PersistActionPool.php
@@ -40,66 +40,66 @@ class PersistActionPool {
 	private $persistSupplyJobs = array();
 	private $emptyPersistActions = array();
 	private $frozen;
-	
+
 	public function __construct(ActionQueue $actionQueue) {
 		$this->actionQueue = $actionQueue;
 	}
-	
+
 	public function containsAction($entity) {
 		ArgUtils::assertTrue(is_object($entity));
 		return isset($this->persistActions[spl_object_hash($entity)]);
 	}
-	
+
 	private function findAction($entity) {
 		ArgUtils::assertTrue(is_object($entity));
 		$objHash = spl_object_hash($entity);
 		if (isset($this->persistActions[$objHash])) {
 			return $this->persistActions[$objHash];
 		}
-		
+
 		$em = $this->actionQueue->getEntityManager();
 		$persistenceContext = $em->getPersistenceContext();
-		
+
 		if ($entity instanceof EntityProxy
 				&& !$persistenceContext->getEntityProxyManager()->isProxyInitialized($entity)) {
 			$entityInfo = $persistenceContext->getEntityInfo($entity, $em->getEntityModelManager());
 			return new UninitializedPersistAction($entityInfo->getEntityModel(), $entityInfo->getId());
 		}
-		
+
 		return null;
 	}
-	
+
 	public function getAction($entity) {
 		if (null !== ($persistAction = $this->findAction($entity))) {
 			return $persistAction;
 		}
-		
+
 		$em = $this->actionQueue->getEntityManager();
 		$persistenceContext = $em->getPersistenceContext();
 		$entityInfo = $persistenceContext->getEntityInfo($entity, $em->getEntityModelManager());
-		
+
 		if ($entityInfo->getState() == EntityInfo::STATE_MANAGED) {
 			throw new IllegalStateException('No PersistAction available for passed entity.');
 		}
-		
+
 		throw new PersistenceOperationException('No valid persist action for '
 				. $entityInfo->getState() . ' entity: ' . $entityInfo->toEntityString());
 	}
-	
+
 	public function removeAction($entity) {
 		ArgUtils::assertTrue(is_object($entity));
 		$objHash = spl_object_hash($entity);
 		if (!isset($this->persistActions[$objHash])) return;
-		
+
 		IllegalStateException::assertTrue(!$this->frozen);
-		
+
 		$persistAction = $this->persistActions[$objHash];
-		
+
 		$persistAction->disable();
 		$this->actionQueue->remove($persistAction);
 		unset($this->persistActions[$objHash]);
 		unset($this->unsuppliedPersistActions[$objHash]);
-		
+
 		$persistenceContext = $this->actionQueue->getEntityManager()->getPersistenceContext();
 		if ($persistAction->isNew()) {
 			$persistenceContext->detachEntityObj($entity);
@@ -117,30 +117,30 @@ class PersistActionPool {
 		if (null !== ($persistAction = $this->findAction($entity))) {
 			return $persistAction;
 		}
-		
+
 		IllegalStateException::assertTrue(!$this->frozen);
-				
+
 		$em = $this->actionQueue->getEntityManager();
 		$persistenceContext = $em->getPersistenceContext();
-				
-		$entityInfo = $persistenceContext->getEntityInfo($entity, 
+
+		$entityInfo = $persistenceContext->getEntityInfo($entity,
 				$em->getEntityModelManager());
 		$entityModel = $entityInfo->getEntityModel();
-		
+
 		$prePersistEvent = null;
 		return $this->createPersistAction($entity, $entityInfo);
 	}
-	
+
 	private function registerPersistAction($entity, PersistAction $persistAction): void {
 		$objHash = spl_object_hash($entity);
 		$this->persistActions[$objHash] = $persistAction;
 		$this->unsuppliedPersistActions[$objHash] = $persistAction;
 		$this->actionQueue->add($persistAction);
 	}
-	
+
 	private function createPersistAction($entity, EntityInfo $entityInfo, ?LifecycleEvent &$prePersistEvent = null) {
 		$entityModel = $entityInfo->getEntityModel();
-		
+
 		switch ($entityInfo->getState()) {
 			case EntityInfo::STATE_NEW:
 				$persistAction = new InsertPersistAction($this->actionQueue, $entityModel,
@@ -148,17 +148,17 @@ class PersistActionPool {
 				$this->registerPersistAction($entity, $persistAction);
 				$this->manage($persistAction);
 				return $persistAction;
-		
+
 			case EntityInfo::STATE_MANAGED:
 				if (!$entityInfo->hasId()) {
 					throw new IllegalStateException('Unable to update entity with unknown id: '
 							. $entityInfo->toEntityString());
 				}
-				$persistAction = new UpdatePersistAction($this->actionQueue, $entityModel, $entityInfo->getId(), 
+				$persistAction = new UpdatePersistAction($this->actionQueue, $entityModel, $entityInfo->getId(),
 						$entity, $this->createActionMeta($entityModel, $entityInfo));
 				$this->registerPersistAction($entity, $persistAction);
 				return $persistAction;
-		
+
 			case EntityInfo::STATE_REMOVED:
 				if (!$entityInfo->hasId()) {
 					throw new IllegalStateException('Unable to update entity with unknown id: '
@@ -169,10 +169,10 @@ class PersistActionPool {
 				$this->registerPersistAction($entity, $persistAction);
 				$this->manage($persistAction);
 				return $persistAction;
-				
+
 // 				throw new PersistenceOperationException('Can not persist removed entity: '
 // 						. $entityInfo->toEntityString());
-		
+
 			case EntityInfo::STATE_DETACHED:
 				throw new PersistenceOperationException('Can not persist detached entity: '
 						. $entityInfo->toEntityString());
@@ -180,7 +180,7 @@ class PersistActionPool {
 
 		throw new IllegalStateException('Illegal state: ' . $entityInfo->getState());
 	}
-	
+
 	private function createActionMeta(EntityModel $entityModel, EntityInfo $entityInfo) {
 		$actionMeta = $entityModel->createActionMeta($entityInfo->getState() === EntityInfo::STATE_NEW);
 		if ($entityInfo->hasId()) {
@@ -189,91 +189,92 @@ class PersistActionPool {
 		}
 		return $actionMeta;
 	}
-	
+
 	private function getPersistActions() {
 		return $this->persistActions;
 	}
-	
+
 	private function manage(PersistActionAdapter $persistAction, ?LifecycleEvent &$prePersistEvent = null): void {
 		$entityModel = $persistAction->getEntityModel();
 		$entity = $persistAction->getEntityObj();
-		
+
 		$persistenceContext = $this->actionQueue->getEntityManager()->getPersistenceContext();
 		$persistenceContext->manageEntityObj($entity, $entityModel);
-		
+
 		if ($persistAction->hasId()) {
 			$persistenceContext->identifyManagedEntityObj($entity, $persistAction->getId());
-		} else {		
+		} else {
 			$persistAction->executeAtEnd(function () use ($entity, $persistenceContext, $persistAction) {
 				$persistenceContext->identifyManagedEntityObj($persistAction->getEntityObj(), $persistAction->getId());
 				$persistAction->getEntityModel()->getIdDef()->getEntityProperty()
 						->writeValue($entity, $persistAction->getId());
 			});
 		}
-		
-		$that = $this;
-		$persistAction->executeAtEnd(function () use ($that, $persistAction) {
-			$that->actionQueue->announceLifecycleEvent(new LifecycleEvent(LifecycleEvent::POST_PERSIST, 
-					$persistAction->getEntityObj(), $persistAction->getEntityModel(), $persistAction->getId(),
-					$persistAction->getMeta()));
-		});
 
-		// Moved down so the entity has already the state managed when the external listener is called. This is to avoid
-		// problems if the called listener is flushing the EntityManager or persists the same entity again.
-		$this->actionQueue->announceLifecycleEvent(new LifecycleEvent(LifecycleEvent::PRE_PERSIST, $entity,
-				$entityModel, $persistAction->getId(), $persistAction->getMeta()));
+//		$that = $this;
+//		$persistAction->executeAtEnd(function () use ($that, $persistAction) {
+//			$that->actionQueue->announceLifecycleEvent(new LifecycleEvent(LifecycleEvent::POST_PERSIST,
+//					$persistAction->getEntityObj(), $persistAction->getEntityModel(), $persistAction->getId(),
+//					$persistAction->getMeta()));
+//		});
+//
+//		// Moved down so the entity has already the state managed when the external listener is called. This is to avoid
+//		// problems if the called listener is flushing the EntityManager or persists the same entity again.
+//		$this->actionQueue->announceLifecycleEvent(new LifecycleEvent(LifecycleEvent::PRE_PERSIST, $entity,
+//				$entityModel, $persistAction->getId(), $persistAction->getMeta()));
 
 	}
-	
+
 	public function isFrozend() {
 		return $this->frozen;
 	}
-	
-	public function freeze() {
+
+	public function freeze(): void {
 		IllegalStateException::assertTrue(!$this->frozen && empty($this->unsuppliedPersistActions));
-		
+
 		$this->frozen = true;
-			
+
 		foreach ($this->persistSupplyJobs as $supplyJob) {
 			$supplyJob->init();
 		}
 	}
-	
-	public function clear() {
+
+	public function clear(): void {
 		foreach ($this->persistActions as $action) {
 			$this->actionQueue->remove($action);
 		}
-		
+
 		$this->frozen = false;
 		$this->persistActions = array();
 		$this->unsuppliedPersistActions = array();
 		$this->persistSupplyJobs = array();
 		$this->emptyPersistActions = array();
 	}
-	
+
 	public function supply() {
 		IllegalStateException::assertTrue($this->frozen);
-				
+
 		foreach ($this->persistSupplyJobs as $supplyJob) {
 			$supplyJob->execute();
 		}
-		
+
 		$this->persistSupplyJobs = array();
 	}
 
 	public function prepareSupplyJobs(): bool {
 		IllegalStateException::assertTrue(!$this->frozen);
-		
+
 		if (empty($this->unsuppliedPersistActions)) return false;
-		
+
 		do {
 			$updatePersistActions = array();
-			
+
 			while (null !== ($persistAction = array_pop($this->unsuppliedPersistActions))) {
 				if ($persistAction->isDisabled()) continue;
-				
+
 				if ($persistAction->isNew()) {
 					$this->refreshSupplyJob($this->persistSupplyJobs[] = new PersistSupplyJob($persistAction));
+					$updatePersistActions[] = $persistAction;
 					continue;
 				}
 
@@ -290,15 +291,15 @@ class PersistActionPool {
 				foreach ($updatePersistActions as $updatePersistAction) {
 					$persistOperation->cascade($updatePersistAction->getEntityObj());
 				}
-				
+
 				foreach ($this->persistSupplyJobs as $persistSupplyJob) {
 					$this->refreshSupplyJob($persistSupplyJob);
 				}
-				
+
 				$updatePersistActions = array();
 				foreach ($this->emptyPersistActions as $key => $emptyPersistAction) {
 					if ($emptyPersistAction->isDisabled()) continue;
-		
+
 					if (null !== ($persistSupplyJob = $this->checkDiff($emptyPersistAction))) {
 						$updatePersistActions[] = $emptyPersistAction;
 						unset($this->emptyPersistActions[$key]);
@@ -307,7 +308,7 @@ class PersistActionPool {
 				}
 			}
 		} while (!empty($this->unsuppliedPersistActions));
-		
+
 		return true;
 	}
 
@@ -319,42 +320,42 @@ class PersistActionPool {
 		$entityModel = $persistAction->getEntityModel();
 		$entity = $persistAction->getEntityObj();
 		$hasher = new ValueHashColFactory($entityModel, $persistAction->getActionQueue()->getEntityManager());
-	
+
 		$values = array();
 		$valueHashCol = $hasher->create($entity, $values);
 		$oldValueHashCol = $this->actionQueue->getEntityManager()->getPersistenceContext()
 				->getValueHashColByEntityObj($entity);
-		
+
 		if ($valueHashCol->matches($oldValueHashCol)) {
 			return null;
 		}
-		
+
 		$supplyJob = new PersistSupplyJob($persistAction, $oldValueHashCol);
 		$supplyJob->setValues($values);
 		$supplyJob->setValueHashCol($valueHashCol);
-		
+
 		$supplyJob->prepare();
 		return $supplyJob;
 	}
-	
+
 	private function refreshSupplyJob(PersistSupplyJob $supplyJob) {
 		if ($supplyJob->isDisabled()) return;
-		
+
 		$persistAction = $supplyJob->getPersistAction();
-		
+
 		$entityModel = $persistAction->getEntityModel();
 		$entity = $persistAction->getEntityObj();
-		
+
 		$hasher = new ValueHashColFactory($entityModel, $this->actionQueue->getEntityManager());
 		$values = array();
 		$valueHashCol = $hasher->create($entity, $values);
 
 // 		cascade could destroy this
 // 		if ($valueHashes === $supplyJob->getValueHashes()) return;
-		
+
 		$supplyJob->setValues($values);
 		$supplyJob->setValueHashCol($valueHashCol);
-		
+
 		$supplyJob->prepare();
 	}
 
@@ -364,11 +365,12 @@ class PersistActionPool {
 	 */
 	private function announceUpdateLifecylcEvents(array $persistActions): bool {
 		$callbacksInvoked = false;
-		
+
 		foreach ($persistActions as $persistAction) {
 			if ($persistAction->isDisabled()) continue;
 
-			if ($this->actionQueue->announceLifecycleEvent(new LifecycleEvent(LifecycleEvent::PRE_UPDATE,
+			if ($this->actionQueue->announceLifecycleEvent(new LifecycleEvent(
+					($persistAction->isNew() ? LifecycleEvent::PRE_PERSIST : LifecycleEvent::PRE_UPDATE),
 					$persistAction->getEntityObj(), $persistAction->getEntityModel(), $persistAction->getId(),
 					$persistAction->getMeta()))) {
 				$callbacksInvoked = true;
@@ -376,12 +378,13 @@ class PersistActionPool {
 
 			$that = $this;
 			$persistAction->executeAtEnd(function () use ($that, $persistAction) {
-				$that->actionQueue->announceLifecycleEvent(new LifecycleEvent(LifecycleEvent::POST_UPDATE,
+				$that->actionQueue->announceLifecycleEvent(new LifecycleEvent(
+						($persistAction->isNew() ? LifecycleEvent::POST_PERSIST : LifecycleEvent::POST_UPDATE),
 						$persistAction->getEntityObj(), $persistAction->getEntityModel(), $persistAction->getId(),
 						$persistAction->getMeta()));
-			});			
-		}	
-		
+			});
+		}
+
 		return $callbacksInvoked;
 	}
 }
